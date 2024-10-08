3bd1f92f9cf90c64c829ea01967b656743c5ae85
Swapnil Pote <138121134+poteswapnil555@users.noreply.github.com>
2024-10-08 18:21:37 +0530

    Add files via upload

---
+++ Netflix_Clone_ Using_(TMDb)_API/src/Components/Home/Home.jsx
@@ -0,0 +1,115 @@
+import React, { useEffect, useState } from "react";
+import "./Home.css";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { BiPlay } from "react-icons/bi"
+import { AiOutlinePlus } from "react-icons/ai"
+
+const apiKey = "de2e77f570c8392c0ce5b451128cd4c0";
+const url = "https://api.themoviedb.org/3";
+const imgUrl = "https://image.tmdb.org/t/p/original";
+const upcoming = "upcoming";
+const nowPlaying = "now_playing";
+const popular = "popular";
+const topRated = "top_rated";
+
+const Card = ({ img }) => <img className="card" src={img} alt="cover" />;
+
+const Row = ({ title, arr = [{}] }) => (
+  <div className="row">
+    <h2>{title}</h2>
+    <div>
+      {arr.map((item, index) => (
+        <Card key={index} img={`${imgUrl}/${item.poster_path}`} />
+      ))}
+    </div>
+  </div>
+);
+
+const Home = () => {
+  const [upcomingMovies, setUpcomingMovies] = useState([]);
+  const [nowPlayingMovies, setNowPlayingMovies] = useState([]);
+  const [popularMovies, setPopularMovies] = useState([]);
+  const [topRatedMovies, setTopRatedMovies] = useState([]);
+  const [genre, setGenre] = useState([]);
+
+  useEffect(() => {
+    const fetchUpcoming = async () => {
+      const {
+        data: { results },
+      } = await axios.get(`${url}/movie/${upcoming}?api_key=${apiKey}&page=2`);
+      setUpcomingMovies(results);
+    };
+
+    const fetchNowPlaying = async () => {
+      const {
+        data: { results },
+      } = await axios.get(`${url}/movie/${nowPlaying}?api_key=${apiKey}&page=3`);
+      setNowPlayingMovies(results);
+    };
+    const fetchPopular = async () => {
+      const {
+        data: { results },
+      } = await axios.get(`${url}/movie/${popular}?api_key=${apiKey}&page=4`);
+      setPopularMovies(results);
+    };
+    const fetchTopRated = async () => {
+      const {
+        data: { results },
+      } = await axios.get(`${url}/movie/${topRated}?api_key=${apiKey}&page=5`);
+      setTopRatedMovies(results);
+    };
+    const getAllGenre = async () => {
+      const {
+        data: { genres },
+      } = await axios.get(`${url}/genre/movie/list?api_key=${apiKey}&page=6`);
+      setGenre(genres);
+      console.log(genres);
+    };
+
+    getAllGenre();
+    fetchUpcoming();
+    fetchNowPlaying();
+    fetchPopular();
+    fetchTopRated();
+  }, []);
+
+  return (
+    <section className="home">
+      <div
+        className="banner"
+        style={{
+          backgroundImage: popularMovies[1]
+            ? `url(${`${imgUrl}/${popularMovies[1].poster_path}`})`
+            : "rgb(16, 16, 16)",
+        }}
+      >
+        
+        {popularMovies[1] && <h1>{popularMovies[1].original_title}</h1>}
+        {popularMovies[1] && <p>{popularMovies[1].overview}</p>}
+        
+        <div>
+          <button><BiPlay /> Play </button>
+          <button>My List <AiOutlinePlus /> </button>
+        </div>
+     
+     
+      </div>
+
+      <Row title={"Upcoming"} arr={upcomingMovies} />
+      <Row title={"Now Playing"} arr={nowPlayingMovies} />
+      <Row title={"Popular"} arr={popularMovies} />
+      <Row title={"Top Rated"} arr={topRatedMovies} />
+
+      <div className="genreBox">
+        {genre.map((item) => (
+          <Link key={item.id} to={`/genre/${item.id}`}>
+            {item.name}
+          </Link>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Home;
